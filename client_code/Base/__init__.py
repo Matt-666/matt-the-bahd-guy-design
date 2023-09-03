79283b307d1b7bc63649598f4238cf7a65f75922
matthew.ademeso+newsletter@elizadeuniversity.edu.ng <matthew.ademeso+newsletter@elizadeuniversity.edu.ng>
2023-09-03 20:45:19 +0000

    Created Base and Home; Edited settings; Deleted Form1

--- client_code/Base/__init__.py
+++ client_code/Base/__init__.py
@@ -1,13 +1,18 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import BaseTemplate
 from anvil import *
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class Form1(Form1Template):
+class Base(BaseTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.label_1.text = "Hello, Matt"
+   
 
     # Any code you write here will run before the form opens.
+
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
+
