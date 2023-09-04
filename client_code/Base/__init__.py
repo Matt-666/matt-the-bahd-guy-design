add774cd1b70d0048001f9f4f796e2d58d02eecc
matthew.ademeso+newsletter@elizadeuniversity.edu.ng <matthew.ademeso+newsletter@elizadeuniversity.edu.ng>
2023-09-04 22:36:09 +0000

    Edited Base

--- client_code/Base/__init__.py
+++ client_code/Base/__init__.py
@@ -13,15 +13,32 @@ class Base(BaseTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.change_sign_in_text()
     self.content_panel.add_component(Home())
 
-    # Any code you write here will run before the form opens.
+  def change_sign_in_text(self):
+    user = anvil.users.get_user()
+    if user:
+      email = user["email"]
+      self.Sign_in.text = email
+    else:
+     self.sign_in.text = "SIGN IN"
+  
+   
+
+  
 
   def link_1_click(self, **event_args):
    """This method is called when the link is clicked"""
    self.content_panel.clear()
    self.content_panel.add_component(Home())
 
+
+  def go_to_home(self):
+    self.content_panel.clear()
+    self.content_panel.add_component((Home))
+
+    
   def My_designs_click(self, **event_args):
     """This method is called when the link is clicked"""
     self.content_panel.clear()
@@ -29,7 +46,14 @@ class Base(BaseTemplate):
 
   def Sign_in_click(self, **event_args):
     """This method is called when the link is clicked"""
+   user = anvil.users.get_users()
+    
+
+
+    
     anvil.users.login_with_form()
+    self.change_sign_in_text()
+    
 
     
 
