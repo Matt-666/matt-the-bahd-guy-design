7d21eb51c62213740333eaf540fe99b326f114f2
matthew.ademeso+newsletter@elizadeuniversity.edu.ng <matthew.ademeso+newsletter@elizadeuniversity.edu.ng>
2023-09-04 11:51:47 +0000

    Edited Home

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -3,10 +3,12 @@ from anvil import *
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from ..Home import Home
 
 class Home(HomeTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.content_panel.add_component(Home())
 
-    # Any code you write here will run before the form opens.
+    
