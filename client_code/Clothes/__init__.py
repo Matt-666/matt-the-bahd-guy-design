ff38218d342752c06b1980f1b439e52b64835b64
matthew.ademeso+newsletter@elizadeuniversity.edu.ng <matthew.ademeso+newsletter@elizadeuniversity.edu.ng>
2023-09-04 15:01:29 +0000

    Created Clothes; Edited Base and Home

--- client_code/Clothes/__init__.py
+++ client_code/Clothes/__init__.py
@@ -1,19 +1,12 @@
-from ._anvil_designer import BaseTemplate
+from ._anvil_designer import ClothesTemplate
 from anvil import *
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from ..Home import Home
 
-class Base(BaseTemplate):
+class Clothes(ClothesTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.content_panel.add_component(Home())
 
     # Any code you write here will run before the form opens.
-
-  def link_1_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    pass
-
--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -3,12 +3,15 @@ from anvil import *
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from ..Home import Home
 
 class Home(HomeTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.content_panel.add_component(Home())
+
+  def view_products_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
+
 
     
