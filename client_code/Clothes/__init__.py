887867b40c89887face87a93627e5ebcae10f634
matthew.ademeso+newsletter@elizadeuniversity.edu.ng <matthew.ademeso+newsletter@elizadeuniversity.edu.ng>
2023-09-07 15:44:14 +0000

    Created ShopItem; Edited Clothes; Deleted Form1

--- client_code/Clothes/__init__.py
+++ client_code/Clothes/__init__.py
@@ -7,13 +7,15 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from ..ShopItem import ShopItem
 
 class Clothes(ClothesTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     self.load_shop()
-
+    self.content_panel.add_componenet(ShopItem(name="Trousers",button_text="Buy for #19299.99",decription="cover your nakedness")
+                                      
   def load_shop(self):
    shop = anvil.server.call("get_shop_details").search()
    
